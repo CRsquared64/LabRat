3372c940c4bd10be147926b6ea765e2d0863cacd
CALLUM <zpac226@live.rhul.ac.uk>
2026-07-22 00:31:24 +0100

    created async scan_ports

--- port-scanner.py
+++ port-scanner.py
@@ -3,6 +3,9 @@ import socket as s
 import ipaddress
 import time
 
+COMMON_PORTS = [21, 22, 23, 25, 53, 80, 81, 110, 135, 143, 443,
+                465, 587, 993, 995, 3389, 5900, 8080, 9100]
+
 class PortScanner():
     def __init__(self, ip, concurrency=500, timeout=1, *args, **kwargs):
         """
@@ -16,7 +19,7 @@ class PortScanner():
         self.timeout = timeout
         self.sem = asyncio.Semaphore(concurrency) # handler for concurrency
 
-    async def check_port(self, ip, port):
+    async def scan_port(self, ip, port):
         async with self.sem: # to ensure we are under concurrency
             try:
                 reader, writer = await asyncio.wait_for(
@@ -28,3 +31,13 @@ class PortScanner():
                 return port
             except (asyncio.TimeoutError, ConnectionRefusedError, OSError):
                 return None
+
+    async def scan_ports(self, ip , ports):
+        start = time.time()
+        tasks = [self.scan_port(ip, p) for p in ports]
+        results = await asyncio.gather(*tasks)
+
+        open_ports = sorted(p for p in results if p is not None)
+        time_elapsed = time.time() - start
+        print(f"Open Ports on {ip}: [{open_ports}]")
+
